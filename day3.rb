83cbcf2bda484e37cac1ef12afc20e26646bf373
Brad Sokol <bsokol@gmail.com>
2021-12-04 15:37:41 -0500

    Day 3

--- day3.rb
+++ day3.rb
@@ -1,6 +1,20 @@
 #! /usr/bin/env ruby
 
-def filter(values)
+def filter(values, position, rating)
+  threshold = (values.count + 1) / 2
+  columns = values.transpose
+  ones = columns[position].count { |e| e == '1' }
+  keep = if rating == :oxygen_generator
+           ones >= threshold ? '1' : '0'
+         else
+           ones < threshold ? '1': '0'
+         end
+  keepers, _ = values.partition { |number| number[position] == keep }
+  keepers
+end
+
+def pretty(values)
+  puts values.map { |v| v.join }.join(' ')
 end
 
 $numbers = File.readlines(ARGV[0]).map { |line| line.chomp.chars }
@@ -17,6 +31,19 @@ epsilon = gamma.map { |e| e == '0' ? '1' : '0' }
 puts gamma.join.to_i(2) * epsilon.join.to_i(2)
 
 values = $numbers.clone.map(&:clone)
-while values.count { |e| !e.nil? } > 1 do
-  values = filter(values)
+position = 0
+while values.count > 1 do
+  values = filter(values, position, :oxygen_generator)
+  position += 1
 end
+oxygen_generator = values.first.join.to_i(2)
+
+values = $numbers.clone.map(&:clone)
+position = 0
+while values.count > 1 do
+  values = filter(values, position, :co2_scrubber)
+  position += 1
+end
+co2_scrubber = values.first.join.to_i(2)
+
+puts oxygen_generator * co2_scrubber

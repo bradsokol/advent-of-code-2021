a01961ac277aee09ba239e99524a5fe6662fb6a7
Brad Sokol <bsokol@gmail.com>
2021-12-13 09:19:26 -0500

    Day 13 completed

--- day13.rb
+++ day13.rb
@@ -16,17 +16,43 @@ File.readlines(ARGV[0]).each do |line|
   end
 end
 
-position = folds.first[1]
-deletions = []
-paper.each do |(x, y)|
-  next if x < position
+folds.each_with_index do |(direction, position), i|
+  deletions = []
+  paper.each do |(x, y)|
+    if direction == 'x'
+      next if x < position
 
-  new_x = position - (x - position)
-  unless paper.find_index([new_x, y])
-    paper << [new_x, y]
+      new_x = position - (x - position)
+      unless paper.find_index([new_x, y])
+        paper << [new_x, y]
+      end
+    else
+      next if y < position
+
+      new_y = position - (y - position)
+      unless paper.find_index([x, new_y])
+        paper << [x, new_y]
+      end
+    end
+    deletions << [x, y]
   end
-  deletions << [x, y]
-end
-deletions.each { |d| paper.delete(d) }
+  deletions.each { |d| paper.delete(d) }
 
-puts paper.size
+  puts paper.size if i == 0
+end
+
+min_x = min_y = 999_999
+max_x = max_y = 0
+paper.each do |x, y|
+  min_x = [x, min_x].min
+  min_y = [y, min_y].min
+  max_x = [x, max_x].max
+  max_y = [y, max_y].max
+end
+
+(min_y..max_y).each do |y|
+  (min_x..max_x).each do |x|
+    print (paper.find_index([x, y]) ? '#' : ' ')
+  end
+  print "\n"
+end

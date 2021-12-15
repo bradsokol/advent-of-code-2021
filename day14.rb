1e13c1b091cf6d2de99d2b730c8f8b207997ae68
Brad Sokol <bsokol@gmail.com>
2021-12-15 17:01:01 -0500

    Progress on Day 14 part 2

--- day14.rb
+++ day14.rb
@@ -1,5 +1,7 @@
 #! /usr/bin/env ruby
 
+require 'pry-nav'
+
 lines = File.readlines(ARGV[0])
 polymer = lines[0].chomp.chars
 rules = {}
@@ -20,4 +22,40 @@ end
 end
 
 min, max = polymer.tally.values.minmax
+puts max - min
+
+polymer = Hash.new(0)
+lines[0].chomp.chars.each_cons(2) do |pair|
+  polymer[pair.join] += 1
+end
+
+polymer = Hash.new(0)
+'NCBH'.chars.each_cons(2) do |pair|
+  polymer[pair.join] += 1
+end
+
+40.times do |i|
+  new_polymer = Hash.new(0)
+  polymer.each do |pair, count|
+    left, right = pair.chars
+    insertion = rules[pair]
+
+    new_polymer[[left, insertion].join] += count
+
+    new_polymer[[insertion, right].join] += count
+
+    # if new_polymer.key?(pair)
+    #   new_polymer[pair] -= count
+    # end
+  end
+  polymer = new_polymer
+  binding.pry
+
+  if i == 9
+    min, max = polymer.values.minmax
+    puts max - min
+  end
+end
+
+min, max = polymer.values.minmax
 puts max - min

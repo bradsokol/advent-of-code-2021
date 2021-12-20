78b158280b2aaf779202fdf18a7d264fe2c57291
Brad Sokol <bsokol@gmail.com>
2021-12-19 20:27:53 -0500

    Add testing

--- day1.rb
+++ day1.rb
@@ -1,5 +1,44 @@
 #! /usr/bin/env ruby
 
+require 'minitest/autorun'
+
+class TestWindowSum < MiniTest::Test
+  def setup
+    @input = <<~EOS
+      199
+      200
+      208
+      210
+      200
+      207
+      240
+      269
+      260
+      263
+    EOS
+  end
+
+  def test_input_parsing
+    depths = parse_input(@input.split)
+
+    assert_equal(10, depths.size)
+    assert_equal(Integer, depths.first.class)
+    assert_equal(199, depths.first)
+    assert_equal(263, depths.last)
+  end
+
+  def test_window_sum
+    depths = parse_input(@input.split)
+    sum = window_sum(depths, 1)
+
+    assert_equal(7, sum)
+  end
+end
+
+def parse_input(input)
+  input.map(&:to_i)
+end
+
 def window_sum(data, size)
   last = data[0..size - 1].reduce(:+)
   (1..data.length - size).reduce(0) do |count, i|
@@ -10,7 +49,7 @@ def window_sum(data, size)
   end
 end
 
-depths = File.readlines(ARGV[0]).map(&:to_i)
+depths = parse_input(File.readlines(ARGV[0]))
 
 puts window_sum(depths, 1)
 puts window_sum(depths, 3)
